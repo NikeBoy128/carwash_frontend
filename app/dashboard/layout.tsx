3d6cd0d8305ce1a9781b8bd519b9fcd45df8b4a5
NikeBoy128 <haroldmosquera739@gmail.com>
2024-09-20 11:18:54 -0500

    Refactor login form inputs and layout components

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,87 @@
+import React, { ReactNode } from "react";
+import Link from "next/link";
+import {
+  HiOutlineUsers,
+  HiOutlineChartBar,
+  HiOutlineDocumentText,
+  HiOutlineShoppingCart,
+  HiOutlineUserGroup,
+  HiOutlineTruck,
+} from "react-icons/hi2";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+interface AdminDashboardProps {
+  children: ReactNode;
+}
+
+export default function AdminDashboard({ children }: AdminDashboardProps) {
+  return (
+    <div className="min-h-screen flex">
+      <aside className="w-64 h-screen shadow-lg">
+        <div className="p-6">
+          <h1 className="text-2xl font-bold text-gray-800 dark:text-white">
+            Admin Panel
+          </h1>
+        </div>
+        <nav className="mt-6">
+          <Link
+            href="#"
+            className="flex items-center px-6 py-3 text-gray-700 dark:text-gray-200 transition-colors duration-200 hover:text-black"
+          >
+            <HiOutlineUsers className="w-5 h-5 mr-3" />
+            <span className="font-medium">Users</span>
+          </Link>
+          <Accordion type="single" collapsible className="w-full">
+            <AccordionItem value="reports" className="border-none">
+              <AccordionTrigger className="flex items-center px-6 py-3 text-gray-600 dark:text-gray-400 transition-colors duration-200 hover:text-black">
+                <div className="flex items-center">
+                  <HiOutlineChartBar className="w-5 h-5 mr-3" />
+                  <span className="font-medium">Reports</span>
+                </div>
+              </AccordionTrigger>
+              <AccordionContent>
+                <Link
+                  href="#"
+                  className="flex items-center pl-14 py-2 text-sm text-gray-600 dark:text-gray-400 transition-colors duration-200 hover:text-black"
+                >
+                  <HiOutlineShoppingCart className="w-4 h-4 mr-2" />
+                  Sales
+                </Link>
+                <Link
+                  href="#"
+                  className="flex items-center pl-14 py-2 text-sm text-gray-600 dark:text-gray-400 transition-colors duration-200 hover:text-black"
+                >
+                  <HiOutlineUserGroup className="w-4 h-4 mr-2" />
+                  Employees
+                </Link>
+                <Link
+                  href="#"
+                  className="flex items-center pl-14 py-2 text-sm text-gray-600 dark:text-gray-400 transition-colors duration-200 hover:text-black"
+                >
+                  <HiOutlineTruck className="w-4 h-4 mr-2" />
+                  Vehicles
+                </Link>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+          <Link
+            href="#"
+            className="flex items-center px-6 py-3 mt-2 text-gray-600 dark:text-gray-400 transition-colors duration-200 hover:text-black"
+          >
+            <HiOutlineDocumentText className="w-5 h-5 mr-3" />
+            <span className="font-medium">Invoices</span>
+          </Link>
+        </nav>
+      </aside>
+
+      {/* Main Content */}
+      <div className="flex-1 p-10">{children}</div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
 import LoginForm from "@/components/login";
-
-
+import Image from "next/image";
 export default function Home() {
   return (
-
-    <LoginForm />
-    
+    <div className="flex h-screen">
+      <div className="w-1/2  flex items-center justify-center bg-blue-950">
+        <Image
+          src="/logo.webp"
+          width={500}
+          height={500}
+          alt="Picture of the author"
+        />
+      </div>
+      <div className="w-1/2 flex items-center justify-center">
+        <LoginForm />
+      </div>
+    </div>
   );
 }

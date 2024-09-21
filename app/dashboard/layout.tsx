68ea2b6381d36cde2a45f7c841ebe8afbafa578b
NikeBoy128 <haroldmosquera739@gmail.com>
2024-09-21 08:28:21 -0500

    Refactor dashboard layout and add new concept link

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -7,6 +7,7 @@ import {
   HiOutlineShoppingCart,
   HiOutlineUserGroup,
   HiOutlineTruck,
+  HiOutlineSquaresPlus,
 } from "react-icons/hi2";
 
 import {
@@ -15,6 +16,7 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { RiDashboard3Line } from "react-icons/ri";
 
 interface AdminDashboardProps {
   children: ReactNode;
@@ -30,6 +32,13 @@ export default function AdminDashboard({ children }: AdminDashboardProps) {
           </h1>
         </div>
         <nav className="mt-6">
+          <Link
+            href="#"
+            className="flex items-center px-6 py-3 text-gray-700 dark:text-gray-200 transition-colors duration-200 hover:text-black"
+          >
+            <RiDashboard3Line className="w-5 h-5 mr-3" />
+            <span className="font-medium">Dashnoard</span>
+          </Link>
           <Link
             href="#"
             className="flex items-center px-6 py-3 text-gray-700 dark:text-gray-200 transition-colors duration-200 hover:text-black"
@@ -77,10 +86,16 @@ export default function AdminDashboard({ children }: AdminDashboardProps) {
             <HiOutlineDocumentText className="w-5 h-5 mr-3" />
             <span className="font-medium">Invoices</span>
           </Link>
+
+          <Link
+            href="#"
+            className="flex items-center px-6 py-3 mt-2 text-gray-600 dark:text-gray-400 transition-colors duration-200 hover:text-black"
+          >
+            <HiOutlineSquaresPlus className="w-5 h-5 mr-3" />
+            <span className="font-medium">concepts</span>
+          </Link>
         </nav>
       </aside>
-
-      {/* Main Content */}
       <div className="flex-1 p-10">{children}</div>
     </div>
   );
